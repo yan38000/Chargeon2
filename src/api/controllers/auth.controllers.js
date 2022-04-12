ed16664da941a646d78d1ffcc373117610cac3c9
yan38000 <61967417+yan38000@users.noreply.github.com>
2022-04-12 11:54:40 +0200

    login - register

--- src/api/controllers/auth.controllers.js
+++ src/api/controllers/auth.controllers.js
@@ -8,7 +8,34 @@
 //---------------------------------
 const {schemaLoginValidate} = require('../validations/auth.validations');
 const User = require('../models/auth.models');
+const bcrypt = require('bcrypt');
+const req = require('express/lib/request');
 
+
+module.exports.register = async(req , res) => {
+    const user = new User({
+        name : req.body.name,
+        password : req.body.password
+    });
+    try {
+        const saveUser = await user.save();
+    } catch (error) {
+        res.status(400).send(error);
+    };
+
+}
 module.exports.login = async(req , res) => {
-    res.send('ok login');
+    //validation 
+    const {error} = schemaLoginValidate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+        //check si le name existe
+        const nameExist = await User.findOne({name : req.body.name});
+        if(!nameExist) return res.status(400).send("Le nom est invalide");
+
+        //check si le password est correct
+        const validPass = await bcrypt.compare(req.body.password , user.password);
+        if(!validPass) return res.status(400).send("Le mot de passe est invalide");
+
+        res.send('logiiin')
 };
--- src/api/routes/auth.routes.js
+++ src/api/routes/auth.routes.js
@@ -9,7 +9,7 @@
 const router = require('express').Router();
 const authControllers = require('../controllers/auth.controllers');
 
-
-router.get('/login' , authControllers.login);
+router.post('/register' , authControllers.register);
+router.post('/login' , authControllers.login);
 
 module.exports = router;
